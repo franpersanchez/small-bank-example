1f3f9748c125b6f91420858eb16b1a7863001f76
Fran Perez <franpersanchez@gmail.com>
2024-11-29 00:17:38 +0100

    add controller for creating user

--- src/main/java/org/example/smallbankexample/infraestructure/rest/controller/UserController.java
+++ src/main/java/org/example/smallbankexample/infraestructure/rest/controller/UserController.java
@@ -1,4 +1,25 @@
 package org.example.smallbankexample.infraestructure.rest.controller;
 
+import org.example.smallbankexample.application.services.UserService;
+import org.example.smallbankexample.domain.models.dto.UserDto;
+import org.example.smallbankexample.domain.models.dto.request.UserRequest;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
 public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping()
+    public UserDto create(@RequestBody UserRequest taskRequest){
+        return userService.createUser(taskRequest);
+    }
 }
