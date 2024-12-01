c78be97dc5cea8acc574936ee1d655442d9e1e5b
Fran Perez <franpersanchez@gmail.com>
2024-12-01 23:08:59 +0100

    improve tests for UserManagementService class (usecases implementation)

--- src/test/java/org/example/smallbankexample/application/services/UserServiceTest.java
+++ src/test/java/org/example/smallbankexample/application/services/UserServiceTest.java
@@ -1,22 +1,21 @@
 package org.example.smallbankexample.application.services;
 
-import org.example.smallbankexample.application.mapper.UserMapper;
+import org.example.smallbankexample.application.mapper.UserDtoMapper;
+import org.example.smallbankexample.application.mapper.UserRequestMapper;
 import org.example.smallbankexample.domain.models.User;
+import org.example.smallbankexample.domain.models.constants.UserConstant;
 import org.example.smallbankexample.domain.models.dto.UserDto;
 import org.example.smallbankexample.domain.models.dto.request.UserRequest;
 import org.example.smallbankexample.domain.ports.port.UserRepositoryPort;
-
 import org.example.smallbankexample.infraestructure.addapters.exceptions.UserException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.springframework.http.HttpStatus;
-
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 
@@ -26,7 +25,10 @@ public class UserServiceTest {
      UserRepositoryPort userRepositoryPort;
 
     @Mock
-    UserMapper userMapper;
+    UserRequestMapper userRequestMapper;
+
+    @Mock
+    UserDtoMapper userDtoMapper;
 
     @InjectMocks
     UserManagementService userManagementService;
@@ -39,48 +41,59 @@ public class UserServiceTest {
     void setUp() {
         MockitoAnnotations.initMocks(this);
 
-        userRequest = new UserRequest("franpersanchez", "franpersanchez@gmail.com", "password1234");
-        userDomain = new User("franpersanchez", "franpersanchez@gmail.com", "password1234");
-        userDto = new UserDto(1L, "franpersanchez", "franpersanchez@gmail.com", null, "No wallets yet");
+        userRequest = new UserRequest("Fran", "franpersanchez@gmail.com", "1234" );
+        userDto = new UserDto(1L, "Fran", "franpersanchez@gmail.com");
+        userDomain = new User(1L, "Fran", "franpersanchez@gmail.com", "1234", null);
 
     }
+
     @Test
     void createUser_success() {
-        when(userRepositoryPort.findByName(any(String.class))).thenReturn(null);
-        when(userRepositoryPort.findByEmail(any(String.class))).thenReturn(null);
-        when(userRepositoryPort.create(any(User.class))).thenReturn(userDomain);
+        when(userRequestMapper.toDomain(userRequest)).thenReturn(userDomain);
+        when(userRepositoryPort.findByName(userRequest.getName())).thenReturn(null);
+        when(userRepositoryPort.findByEmail(userRequest.getEmail())).thenReturn(null);
+        when(userRepositoryPort.create(userDomain)).thenReturn(userDomain);
+        when(userDtoMapper.toDto(userDomain)).thenReturn(userDto);
 
-        UserDto createdUser = userManagementService.createUser(userRequest);
+        UserDto result = userManagementService.createUser(userRequest);
 
-        assertNotNull(createdUser);
-        assertEquals(userDto.getName(), createdUser.getName());
-        assertEquals(userDto.getEmail(), createdUser.getEmail());
+        assertNotNull(result);
+        assertEquals("Fran", result.getName());
+        assertEquals("franpersanchez@gmail.com", result.getEmail());
+
+        verify(userRepositoryPort).create(userDomain);
+        verify(userDtoMapper).toDto(userDomain);
     }
 
     @Test
     void createUser_emailAlreadyExists() {
-        when(userRepositoryPort.findByName("franpersanchez")).thenReturn(null);
-        when(userRepositoryPort.findByEmail("franpersanchez@gmail.com")).thenReturn(userDomain);
+        when(userRequestMapper.toDomain(userRequest)).thenReturn(userDomain);
+        when(userRepositoryPort.findByName(userRequest.getName())).thenReturn(null);
+        when(userRepositoryPort.findByEmail(userRequest.getEmail())).thenReturn(userDomain);
+
 
         UserException exception = assertThrows(UserException.class, () -> {
             userManagementService.createUser(userRequest);
         });
 
         assertEquals(HttpStatus.BAD_REQUEST, exception.getErrorCode());
-        assertEquals("User with email franpersanchez@gmail.com is already registered.", exception.getErrorMessage());
+        String expectedErrorMessage = String.format(UserConstant.USER_HAS_EMAIL_ALREADY_REGISTERED, userRequest.getEmail());
+        assertEquals(expectedErrorMessage, exception.getErrorMessage());
     }
 
     @Test
     void createUser_usernameAlreadyExists() {
-        when(userRepositoryPort.findByName("franpersanchez")).thenReturn(userDomain);
-        when(userRepositoryPort.findByEmail("franpersanchez@gmail.com")).thenReturn(null);
+        when(userRequestMapper.toDomain(userRequest)).thenReturn(userDomain);
+        when(userRepositoryPort.findByName(userRequest.getName())).thenReturn(userDomain);
+        when(userRepositoryPort.findByEmail(userRequest.getEmail())).thenReturn(null);
 
         UserException exception = assertThrows(UserException.class, () -> {
             userManagementService.createUser(userRequest);
         });
 
         assertEquals(HttpStatus.BAD_REQUEST, exception.getErrorCode());
-        assertEquals("User with username franpersanchez is already registered.", exception.getErrorMessage());
+        String expectedErrorMessage = String.format(UserConstant.USER_HAS_USERNAME_ALREADY_REGISTERED, userRequest.getName());
+        assertEquals(expectedErrorMessage, exception.getErrorMessage());
     }
 
 
